e6527537feebc97ad95a9d0db480e9ca8bb800ae
Alex Tong <alexandertongdev@gmail.com>
2022-08-31 08:38:33 -0400

    Add MLP (doesn't seem to improve performance)

--- src/models/components/scatter_net.py
+++ src/models/components/scatter_net.py
@@ -1,5 +1,6 @@
+from typing import Optional
+
 from torch import nn
-from torch_geometric.nn import global_mean_pool
 
 from src.models.scatter import Scatter
 
@@ -20,3 +21,20 @@ class LR(nn.Linear):
     def forward(self, data):
         x = data.x
         return super().forward(x)
+
+
+class MLP(nn.Module):
+    def __init__(self, input_dim, output_dim, hidden_dims: Optional[list] = None):
+        super().__init__()
+        if hidden_dims is None:
+            hidden_dims = [64, 64]
+        hd = [input_dim, *hidden_dims, output_dim]
+        layers = []
+        for i in range(len(hd) - 1):
+            layers.append(nn.Linear(hd[i], hd[i + 1]))
+            layers.append(nn.ReLU())
+        self.model = nn.Sequential(*layers)
+
+    def forward(self, data):
+        x = data.x
+        return self.model(x)
