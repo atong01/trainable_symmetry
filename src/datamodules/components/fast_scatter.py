fe116466c9f542a19dd9997004ab8a0a82fa6ded
Alex Tong <alexandertongdev@gmail.com>
2022-08-31 08:37:55 -0400

    commit
    Add W1 filters

--- src/datamodules/components/fast_scatter.py
+++ src/datamodules/components/fast_scatter.py
@@ -1,14 +1,17 @@
-from src.models.scatter import Scatter, Scatter_Diffuse_Second
+from src.models.scatter2 import Scatter, Scatter_Diffuse_Second, Scatter_W1
 
 
 class FastScatterTransform:
-    def __init__(self, device="cpu", agg="moment", alpha=0):
+    def __init__(self, device="cpu", agg="moment", alpha=0, power=2):
         self.device = device
         self.agg = agg
         self.alpha = alpha
+        self.power = power
+        assert power == 1 or power == 2
+        self.scatter_fn = Scatter if power == 2 else Scatter_W1
 
     def __call__(self, data):
-        data.x = Scatter(data.x.shape[1], agg=self.agg, alpha=self.alpha)(data)
+        data.x = self.scatter_fn(data.x.shape[1], agg=self.agg, alpha=self.alpha)(data)
         return data
 
 
